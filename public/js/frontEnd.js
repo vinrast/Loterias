add43fbfa7b6e340e3b972eb7414b304ecbd94d8
Vincen Yoel Santaella <vinrast@gmail.com>
2017-04-28 21:25:23 -0430

    Arreglando conflictos

--- public/js/frontEnd.js
+++ public/js/frontEnd.js
@@ -59,6 +59,98 @@ var jugadaId=1;
 	});
 }
 
+
+function anularJugada()
+{
+	$("#anularJugada").click(function()
+		{
+
+			var tabla=document.getElementById('tablaJugadas');
+			var checks=tabla.getElementsByTagName("input");
+			var longitud=checks.length;
+			var ids=[];
+			//alert(longitud); la longitud es correcta
+			var c=0;
+
+			$.each(checks,function()
+				{
+					if($(this).prop('checked')==true)
+					{
+						c=c+1;
+						ids.push($(this).attr('data-idj'));
+
+					}
+				
+				});
+			if (c==0 && longitud>0) 
+			{
+				// swal({
+				// 		title:'Debe seleccionar las jugadas que desea anular!!',//Contenido del modal
+				// 		text: '<p style="font-size: 1.5em;">'+''+'</p>',
+				// 		timer:2000,//Tiempo de retardo en ejecucion del modal
+				// 		type: "warning",
+				// 		showConfirmButton:false,//Eliminar boton de confirmacion
+				// 		html: true
+				// 	});
+			}
+			else
+			{
+
+					for (var i = 0; i < ids.length; i++)
+					 {
+						
+					 	valor=valor-parseInt($("#apuesta"+ids[i]).text());
+						$('#filaJugada'+ids[i]).remove();
+						
+					}
+
+					$("#chekJugadas").prop('checked',false);
+
+					$('#dineroTotal').text(valor);
+					
+
+			}
+
+
+		});
+
+
+}
+
+
+function seleccionarJugada()//cambia el status de los cheks de las jugadas
+{
+
+$("#chekJugadas").change(function()
+	{
+		var tabla=document.getElementById('tablaJugadas');
+		var checks=tabla.getElementsByTagName("input");
+		var estado=$(this).prop('checked');
+		//alert(checks.length);
+
+		if (checks.length>0) 
+		{
+
+			$.each(checks,function(i)
+			{
+
+				if ($(this).prop('checked')!=estado) 
+				{
+
+					$(this).prop('checked',estado);
+				}
+			});
+
+		}
+		else
+		{
+			$("#chekJugadas").prop('checked',false);
+		}
+
+		
+	});
+}
+
 function AgregarJugada()
 {
 
@@ -66,260 +158,154 @@ function AgregarJugada()
 		{
 			var sorteos=document.getElementById('sorteosDisponibles');
 			var checks=sorteos.getElementsByTagName("input");
-			
+			var jugada=new Object();
+			var sorteos_=[];
+			var jugadas=[];
+			var apuestas=[];
+
+
 			var c=0;
 			var sorteo=null;
 			$.each(checks, function(i)
-
-
-			function anularJugada()
 			{
-				$("#anularJugada").click(function()
-					{
-
-						var tabla=document.getElementById('tablaJugadas');
-						var checks=tabla.getElementsByTagName("input");
-						var longitud=checks.length;
-						var ids=[];
-						//alert(longitud); la longitud es correcta
-						var c=0;
-
-						$.each(checks,function()
-							{
-								if($(this).prop('checked')==true)
-								{
-									c=c+1;
-									ids.push($(this).attr('data-idj'));
-
-								}
-							
-							});
-						if (c==0 && longitud>0) 
-						{
-							// swal({
-							// 		title:'Debe seleccionar las jugadas que desea anular!!',//Contenido del modal
-							// 		text: '<p style="font-size: 1.5em;">'+''+'</p>',
-							// 		timer:2000,//Tiempo de retardo en ejecucion del modal
-							// 		type: "warning",
-							// 		showConfirmButton:false,//Eliminar boton de confirmacion
-							// 		html: true
-							// 	});
-						}
-						else
-						{
-
-								for (var i = 0; i < ids.length; i++)
-								 {
-									
-								 	valor=valor-parseInt($("#apuesta"+ids[i]).text());
-									$('#filaJugada'+ids[i]).remove();
-									
-								}
-
-								$("#chekJugadas").prop('checked',false);
-
-								$('#dineroTotal').text(valor);
-								
-
-						}
-
-
+				
+				if ($(this).prop("checked")==true )
+				{
+					c=c+1;
+					//alert($(this).attr('data-descripcion'));
+					sorteos_.push($(this).attr('data-descripcion'));
+				}
+				
+			});
+			
+			if (c==0)//si no se seleccionan sorteos
+			{
+				swal({
+						title:'Debe seleccionar un sorteo!!',//Contenido del modal
+						text: '<p style="font-size: 1.5em;">'+''+'</p>',
+						timer:2000,//Tiempo de retardo en ejecucion del modal
+						type: "warning",
+						showConfirmButton:false,//Eliminar boton de confirmacion
+						html: true
 					});
-
-
+				
 			}
-
-
-			function seleccionarJugada()//cambia el status de los cheks de las jugadas
+			else if(c>=1)//si se seleccionan sorteos
 			{
+				
+				var sorteos=document.getElementById('tripleta');
+				var dupletas=sorteos.getElementsByTagName("input");
+				var tripleta=" ";
+				c=0;
 
-				$("#chekJugadas").change(function()
+				$.each(dupletas, function(i)
+				{
+					
+					if ($(this).val()!='')
 					{
-						var tabla=document.getElementById('tablaJugadas');
-						var checks=tabla.getElementsByTagName("input");
-						var estado=$(this).prop('checked');
-						//alert(checks.length);
-
-						if (checks.length>0) 
+						c=c+1;
+						if (c==1) 
 						{
-
-							$.each(checks,function(i)
-							{
-
-								if ($(this).prop('checked')!=estado) 
-								{
-
-									$(this).prop('checked',estado);
-								}
-							});
-
+							tripleta=tripleta+$(this).val();
 						}
 						else
 						{
-							$("#chekJugadas").prop('checked',false);
+							tripleta=tripleta+'-'+$(this).val();
 						}
-
 						
-					});
-			}
+					}
+					
+					
+				});
 
 
 
-			function AgregarJugada()
-			{
-
-				$("#ADD").click(function()
+				if (c==0) 
 					{
-						var sorteos=document.getElementById('sorteosDisponibles');
-						var checks=sorteos.getElementsByTagName("input");
-						var jugada=new Object();
-						var sorteos_=[];
-						var jugadas=[];
-						var apuestas=[];
-
-
-						var c=0;
-						var sorteo=null;
-						$.each(checks, function(i)
-						{
-							
-							if ($(this).prop("checked")==true )
-							{
-								c=c+1;
-								//alert($(this).attr('data-descripcion'));
-								sorteos_.push($(this).attr('data-descripcion'));
-							}
-							
+						swal({
+								title:'Debe ingresar al menos una dupleta!!',//Contenido del modal
+								text: '<p style="font-size: 1.5em;">'+''+'</p>',
+								timer:2000,//Tiempo de retardo en ejecucion del modal
+								type: "warning",
+								showConfirmButton:false,//Eliminar boton de confirmacion
+								html: true
 						});
 						
-						if (c==0)//si no se seleccionan sorteos
+					}
+				else
+					{
+						// var tripleta_='';
+						// for (var i = 0; i < tripleta.length; i++) 
+						// {
+						// 	if(i>0)
+						// 	{
+						// 		tripleta_=tripleta_+'-'+tripleta[i];
+						// 	}
+						// 	else
+						// 	{
+						// 		tripleta_=tripleta_+tripleta[i];
+						// 	}
+
+						// }
+						//jugada.tripleta=tripleta_;
+						
+						if ($('#Apuesta').val()=='') 
 						{
-							swal({
-									title:'Debe seleccionar un sorteo!!',//Contenido del modal
-									text: '<p style="font-size: 1.5em;">'+''+'</p>',
-									timer:2000,//Tiempo de retardo en ejecucion del modal
-									type: "warning",
-									showConfirmButton:false,//Eliminar boton de confirmacion
-									html: true
-								});
+								swal({
+										title:'Debe indicar la apuesta para la jugada!!',//Contenido del modal
+										text: '<p style="font-size: 1.5em;">'+''+'</p>',
+										timer:2000,//Tiempo de retardo en ejecucion del modal
+										type: "warning",
+										showConfirmButton:false,//Eliminar boton de confirmacion
+										html: true
+									});
+						
 							
 						}
-						else if(c>=1)//si se seleccionan sorteos
+						else
 						{
 							
-							var sorteos=document.getElementById('tripleta');
-							var dupletas=sorteos.getElementsByTagName("input");
-							var tripleta=" ";
-							c=0;
+							//alert(jugada.sorteo+'  '+jugada.tripleta+' Por: '+$('#Apuesta').val());
+							for (var i = 0; i < sorteos_.length; i++) 
+							{
+								
+							
+								$('#tablaJugadas').append(' <tr class="resultadoi" id="filaJugada'+jugadaId+'">  <th><input type="checkbox" class="checkJugada" id="check'+jugadaId+'" data-idj="'+jugadaId+'"/><label for="check'+jugadaId+'" ></label></th>  <th>'+sorteos_[i]+'</th>  <th>'+tripleta+'</th> <th id="apuesta'+jugadaId+'">'+$('#Apuesta').val()+'</th></tr>');
+								valor=valor+parseInt($('#Apuesta').val());
+								jugadaId=jugadaId+1;
+							}
+
+
+							$('#dineroTotal').text(valor);
+							$('#Apuesta').val(" ");
+							$.each(checks, function(i)
+								{
+				
+									if ($(this).prop("checked")==true )
+									{
+										$(this).prop("checked",false);
+									}
+				
+								});
+
 
 							$.each(dupletas, function(i)
 							{
 								
-								if ($(this).val()!='')
-								{
-									c=c+1;
-									if (c==1) 
-									{
-										tripleta=tripleta+$(this).val();
-									}
-									else
-									{
-										tripleta=tripleta+'-'+$(this).val();
-									}
-									
-								}
+								$(this).val(" ");
+								
 								
 								
 							});
-
-
-
-							if (c==0) 
-								{
-									swal({
-											title:'Debe ingresar al menos una dupleta!!',//Contenido del modal
-											text: '<p style="font-size: 1.5em;">'+''+'</p>',
-											timer:2000,//Tiempo de retardo en ejecucion del modal
-											type: "warning",
-											showConfirmButton:false,//Eliminar boton de confirmacion
-											html: true
-									});
-									
-								}
-							else
-								{
-									// var tripleta_='';
-									// for (var i = 0; i < tripleta.length; i++) 
-									// {
-									// 	if(i>0)
-									// 	{
-									// 		tripleta_=tripleta_+'-'+tripleta[i];
-									// 	}
-									// 	else
-									// 	{
-									// 		tripleta_=tripleta_+tripleta[i];
-									// 	}
-
-									// }
-									//jugada.tripleta=tripleta_;
-									
-									if ($('#Apuesta').val()=='') 
-									{
-											swal({
-													title:'Debe indicar la apuesta para la jugada!!',//Contenido del modal
-													text: '<p style="font-size: 1.5em;">'+''+'</p>',
-													timer:2000,//Tiempo de retardo en ejecucion del modal
-													type: "warning",
-													showConfirmButton:false,//Eliminar boton de confirmacion
-													html: true
-												});
-									
-										
-									}
-									else
-									{
-										
-										//alert(jugada.sorteo+'  '+jugada.tripleta+' Por: '+$('#Apuesta').val());
-										for (var i = 0; i < sorteos_.length; i++) 
-										{
-											
-										
-											$('#tablaJugadas').append(' <tr class="resultadoi" id="filaJugada'+jugadaId+'">  <th><input type="checkbox" class="checkJugada" id="check'+jugadaId+'" data-idj="'+jugadaId+'"/><label for="check'+jugadaId+'" ></label></th>  <th>'+sorteos_[i]+'</th>  <th>'+tripleta+'</th> <th id="apuesta'+jugadaId+'">'+$('#Apuesta').val()+'</th></tr>');
-											valor=valor+parseInt($('#Apuesta').val());
-											jugadaId=jugadaId+1;
-										}
-
-
-										$('#dineroTotal').text(valor);
-										$('#Apuesta').val(" ");
-										$.each(checks, function(i)
-											{
-							
-												if ($(this).prop("checked")==true )
-												{
-													$(this).prop("checked",false);
-												}
-							
-											});
-
-
-										$.each(dupletas, function(i)
-										{
-											
-											$(this).val(" ");
-											
-											
-											
-										});
-									}
-								}
-
-
 						}
-						
+					}
 
-					});
+
 			}
+			
+
+		});
+}
 
 
 
